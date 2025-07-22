06554ffe9ec3419f9263aaaf3de4a9fd1dcbe22f
jaycheng17 <jaycheng120@gmail.com>
2025-07-22 16:34:26 +0800

    everything

---
+++ SpringBoot with User Auth/src/main/java/com/testingSpringBoot/testProject/TestingSpringBootWithAuthApplication.java
@@ -0,0 +1,13 @@
+package com.testingSpringBoot.testProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSpringBootWithAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSpringBootWithAuthApplication.class, args);
+	}
+
+}
---
+++ SpringBoot with User Auth/src/test/java/com/testingSpringBoot/testProject/TestingSpringBootWithAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testingSpringBoot.testProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringBootWithAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
